a70621aecde4faf9aa09ec1db43c38c07a41a898
leemzilla15 <leemzilla15@gmail.com>
2026-04-09 19:42:37 -0500

    Update priceHistory.js

--- trading-dashboard/server/services/priceHistory.js
+++ trading-dashboard/server/services/priceHistory.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const KEY = process.env.FINHUB-API-KEY;
+const KEY = process.env.FINHUB_API_KEY;
 const BASE = 'https://finnhub.io/api/v1';
 async function get1HCandles(symbol) {
 return[];
